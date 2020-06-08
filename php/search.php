cef5ba854b1609acfd1b22c2a0596ce638bd561f
Geraldo HernandezGarcia <hernager@flip1.engr.oregonstate.edu>
2020-06-07 20:42:10 -0700

    Remove 'Search' content

--- php/search.php
+++ php/search.php
@@ -9,26 +9,7 @@ $pageTitle = "Search Page";
 include("templates/header.php");	?>
 
 		<div class="container grid">
-			<div>
-				<h1>Search Movies</h1>
-			</div>
-			<div class="search movieDb">
-				<input type="text" class="searchTerm" placeholder="Enter Movie Title">
-				<button type="submit" class="searchButton">
-					<i class="fa fa-search">
-					</i>
-				</button>
-			</div>
-			<div>
-				<h1>Search Actors</h1>
-			</div>
-			<div class="search actorDb">
-				<input type="text" class="searchTerm" placeholder="Enter Actor's First Name">
-				<button type="submit" class="searchButton">
-					<i class="fa fa-search">
-					</i>
-				</button>
-			</div>
+			
 		</div>
 	</div>
 
